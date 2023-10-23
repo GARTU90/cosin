16499d88563393dcc96477ed6aba10d09854fe24
Alrrep <juan.daniel.rangel.avila@gmail.com>
2023-10-23 13:22:08 -0600

    ejercicio 19  2 decimas

--- cosine.py
+++ cosine.py
@@ -2,17 +2,38 @@
 #by gartujuan@gmail.com
 #gnu/gpl license
 #plotting cosine function
+import math
+import matplotlib.pyplot as plt
+def f(x):
+    Y=math.cos(x)
+    return Y
+def g(x):
+    Y=math.sin(x)
+    return Y
 #y = f(x)
 #x= [0,2pi]
 
-N=8
+N=10
 A=0
-B=200
+B=2*3.1416
 dx=(B-A)/N
 values= range(N+1)
 x=A
+X=[]
+Y=[]
+Z=[]
 for jump in values:
-    x = jump * dx
-    print(x)
-    
-    
+    x = A+jump * dx
+    X.append(x)
+
+    y=f(x)
+    z=g(x)
+    Z.append(z)
+    Y.append(y)
+    #x+=dx
+    print(x,y)
+plt.plot(X,Y)
+plt.scatter(X,Y,color='green')
+plt.plot(X,Z)
+plt.scatter(X,Z,color='red') 
+plt.show()    
